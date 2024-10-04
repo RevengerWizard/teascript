f12fd0be614212569b7d8d75b0c9053f5045724b
Wizard <64542915+RevengerWizard@users.noreply.github.com>
2024-10-04 13:37:41 +0200

    Improve `random` module with some more functions

--- src/lib_random.c
+++ src/lib_random.c
@@ -65,31 +65,102 @@ static void random_random(tea_State* T)
     setnumV(T->top++, d);
 }
 
-static void random_range(tea_State* T)
+static void random_bool(tea_State* T)
 {
-    int count = tea_get_top(T);
     PRNGState* rs = (PRNGState*)tea_check_userdata(T, tea_upvalue_index(0));
     U64double u;
-    double d;
     u.u64 = tea_prng_u64d(rs);
-    d = u.d - 1.0;
-    if(count == 2)
+    setboolV(T->top++, (u.d - 1) < 0.5);
+}
+
+static void random_int(tea_State* T)
+{
+    PRNGState* rs = (PRNGState*)tea_check_userdata(T, tea_upvalue_index(0));
+    int32_t a = tea_lib_checkint(T, 0);
+    int32_t b = tea_lib_checkint(T, 1);
+    U64double u;
+    u.u64 = tea_prng_u64d(rs);
+    int32_t n = (int32_t)((u.d - 1) * (b - a + 1)) + a;
+    setnumV(T->top++, (double)n);
+}
+
+static void random_number(tea_State* T)
+{
+    PRNGState* rs = (PRNGState*)tea_check_userdata(T, tea_upvalue_index(0));
+    double a = tea_lib_checknumber(T, 0);
+    double b = tea_lib_checknumber(T, 1);
+    U64double u;
+    u.u64 = tea_prng_u64d(rs);
+    double d = u.d - 1.0;
+    d = d * (b - a) + a;
+    setnumV(T->top++, d);
+}
+
+static void random_range(tea_State* T)
+{
+    PRNGState* rs = (PRNGState*)tea_check_userdata(T, tea_upvalue_index(0));
+    int count = tea_get_top(T);
+    double start, end, step;
+
+    if(count == 1)
     {
-        double r1 = tea_lib_checknumber(T, 0);
-        double r2 = tea_lib_checknumber(T, 1);
-        d = floor(d * (r2 - r1)) + r1;
-        setnumV(T->top++, d);
-        return;
+        if(tvisrange(T->base))
+        {
+            GCrange* r = tea_lib_checkrange(T, 0);
+            start = r->start;
+            end = r->end;
+            step = r->step;
+        }
+        else
+        {
+            start = 0;
+            end = tea_lib_checknumber(T, 0);
+            step = 1;
+        }
     }
-    else if(count == 1)
+    else if(count == 2)
     {
-        double r1, r2;
-        tea_check_range(T, 0, &r1, &r2, NULL);
-        d = floor(d * (r2 - r1)) + r1;
-        setnumV(T->top++, d);
-        return;
+        start = tea_lib_checknumber(T, 0);
+        end = tea_lib_checknumber(T, 1);
+        step = 1;
     }
-    tea_error(T, "Expected two numbers or a range");
+    else if(count == 3)
+    {
+        start = tea_lib_checknumber(T, 0);
+        end = tea_lib_checknumber(T, 1);
+        step = tea_lib_checknumber(T, 2);
+    }
+    else
+    {
+        tea_error(T, "Expected 1, 2, or 3 arguments");
+    }
+
+    if(step == 0)
+    {
+        tea_error(T, "Empty range");
+    }
+
+    U64double u;
+    u.u64 = tea_prng_u64d(rs);
+    double d = u.d - 1.0;
+
+    if(step > 0)
+    {
+        if(start >= end)
+        {
+            tea_error(T, "Empty range");
+        }
+        d = floor(d * ((end - start) / step)) * step + start;
+    }
+    else
+    {
+        if(start <= end)
+        {
+            tea_error(T, "Empty range");
+        }
+        d = ceil(d * ((start - end) / -step)) * step + end;
+    }
+    setnumV(T->top++, d);
 }
 
 static void random_shuffle(tea_State* T)
@@ -132,7 +203,10 @@ static void random_choice(tea_State* T)
 static const tea_Reg random_module[] = {
     { "seed", random_seed, 1, 0 },
     { "random", random_random, 0, 0 },
-    { "range", random_range, 1, 1 },
+    { "bool", random_bool, 0, 0 },
+    { "int", random_int, 2, 0 },
+    { "number", random_number, 2, 0 },
+    { "range", random_range, 1, 2 },
     { "choice", random_choice, 1, 0 },
     { "shuffle", random_shuffle, 1, 0 },
     { NULL, NULL },
